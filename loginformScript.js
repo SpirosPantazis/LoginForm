ccb74c4095ce633a9067c578ad0f225b3b9c957d
Spiros Pantazis <spirosps12@gmail.com>
2022-02-20 14:42:48 +0200

    Change the method of the form in to post

--- loginformScript.js
+++ loginformScript.js
@@ -4,10 +4,9 @@ var userPassword = "123456789";
 function openUrl(){
     var succesName = document.getElementById("nameValue").value;
     var succesPswd = document.getElementById('pswdValue').value;
-    if(userName == succesName && userPassword === succesPswd){ window.open("https://www.coderfoundry.com"); //in a new tab
+    if(userName == succesName && userPassword === succesPswd){ window.open("https://www.coderfoundry.com"); } //in a new tab
         //in the same tab
-        //window.location.href = "https://www.google.com";
-    }      
+        //window.location.href = "https://www.google.com"; 
 }
 
 function showUserNamePswd() {
