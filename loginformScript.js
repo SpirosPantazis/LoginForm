a41ac25a12ab296b857802e1293e2f320861c101
Spiros Pantazis <spirosps12@gmail.com>
2022-02-20 14:23:35 +0200

    Login form with Html Css Javascript change if in one line code

--- loginformScript.js
+++ loginformScript.js
@@ -4,10 +4,7 @@ var userPassword = "123456789";
 function openUrl(){
     var succesName = document.getElementById("nameValue").value;
     var succesPswd = document.getElementById('pswdValue').value;
-    if(userName == succesName && userPassword === succesPswd){
-        //in a new tab
-        window.open("https://www.coderfoundry.com");
-        
+    if(userName == succesName && userPassword === succesPswd){ window.open("https://www.coderfoundry.com"); //in a new tab
         //in the same tab
         //window.location.href = "https://www.google.com";
     }      
