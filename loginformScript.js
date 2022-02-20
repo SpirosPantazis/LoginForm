0b6570bbb8e39b2fe57b6f2190f186d593032bec
Spiros Pantazis <spirosps12@gmail.com>
2022-02-20 15:06:50 +0200

    modified the loginformScript.js file

--- loginformScript.js
+++ loginformScript.js
@@ -4,15 +4,13 @@ var userPassword = "123456789";
 function openUrl(){
     var succesName = document.getElementById("nameValue").value;
     var succesPswd = document.getElementById('pswdValue').value;
-    if(userName == succesName && userPassword === succesPswd){ 
-        //in a new tab
-        window.open("https://www.google.com");
-         //in the same tab
-        //window.location.href = "https://www.google.com"; 
-     }   
+    if(userName == succesName && userPassword === succesPswd) { window.open("https://www.google.com"); } //in a new tab    
 }
 
 function showUserNamePswd() {
     alert("User name: " + userName + " Password: " + userPassword);
 }
+
+//in the same tab
+//window.location.href = "https://www.google.com"; 
 
