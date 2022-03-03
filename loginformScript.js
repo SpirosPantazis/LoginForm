c6d1f870a0f4a9bb8644796c2afa572400b2e368
Spiros Pantazis <spirosps12@gmail.com>
2022-03-03 14:44:08 +0200

    change the url window.open/window.location.href

--- loginformScript.js
+++ loginformScript.js
@@ -2,14 +2,14 @@ var succesName = "SpirosPantazis";
 var succesPswd = "123456789";
 
 function setUserNamePswd(){
-    var userName = document.getElementsByName("fullname")[0].value;
-    var userPassword = document.getElementsByName('password')[0].value;
-     if((userName == succesName) && (userPassword == succesPswd)) { return window.open("https://www.google.com",);} 
-     return alert("Υπαρχει πρόβλημα είτε στο userName είτε στο Password"); 
-    }
-
+   var userName = document.getElementsByName("fullname")[0].value;
+   var userPassword = document.getElementsByName('password')[0].value;
+    if(userName == succesName && userPassword == succesPswd) {  window.location.href = "https://www.google.com";} 
+    return false;
+   }
+     
 function getUserNamePswd() {
-    return alert("User name: " + userName + " Password: " + userPassword);
+    return alert("User name: " + succesName + " Password: " + succesPswd);
 }
     
 //window.open("https://www.google.com"); } in a new tab    
