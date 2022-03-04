52119356a7d7f0953840738b3627a6b68b000ba6
Spiros Pantazis <spirosps12@gmail.com>
2022-03-04 17:52:25 +0200

    comments

--- loginformScript.js
+++ loginformScript.js
@@ -4,17 +4,15 @@ var succesPswd = "123456789";
 function setUserNamePswd(){
    var userName = document.getElementsByName("fullname")[0].value;
    var userPassword = document.getElementsByName('password')[0].value;
-    if(userName == succesName && userPassword == succesPswd) {  window.location.href = "https://www.google.com";} 
-    return false;
-   }
-     
+     if(userName == succesName && userPassword == succesPswd) {  window.location.href = "https://www.google.com";} 
+     return false;
+    //window.open("https://www.google.com"); } in a new tab    
+    //window.location.href = "https://www.google.com"; //in the same tab 
+}
+
 function getUserNamePswd() {
     return alert("User name: " + succesName + " Password: " + succesPswd);
 }
     
-//window.open("https://www.google.com"); } in a new tab    
 
-
-//in the same tab
-//window.location.href = "https://www.google.com"; 
 
